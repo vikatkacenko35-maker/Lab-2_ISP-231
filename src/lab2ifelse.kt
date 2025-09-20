f14c0174afb4d614fe532f79300f2b5422bc8442
Danara <chaykalak.d08@gmail.com>
2025-09-20 10:44:56 +0300

    commit change file

--- src/lab2ifelse.kt
+++ src/lab2ifelse.kt
@@ -1,5 +1,26 @@
-    fun main(){
-        var num = 10
-        println(num)
-        println(num + 1)
+fun main() {
+    //print("Введите первое число: ")
+   // val num1 = readln().toInt()
+    //print("Введите второе число: ")
+    //val num2 = readln().toInt()
+    //val max = if (num1 > num2) num1 else num2
+   //println(max)
+    //if (num1 > num2) {
+    //    println("num 1 > num2")
+    //} else {
+        //println("num2 > num1")
+    //}
+    //val age = readln().toInt()
+    //val status = if (age >= 18) "Сoвершенолетний" else "несовершенолетний"
+    //println(status)
+    print("Введите урон: ")
+    val damage = readln().toInt()
+    print("Щит активен? ")
+    val active = readln()
+    if (active == "Да"){
+        println("Полученный урон ${damage / 2}")
+    } else{
+        println("Полученный урон ${damage}")
     }
+
+}
